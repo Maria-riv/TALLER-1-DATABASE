4dc9520d9eaa9ce3c3db257019b52c1d61bfda99
Maria-riv <maria.rivera01@alumnos.ucn.cl>
2022-10-07 20:35:34 -0300

    ACTUALIZACIÓN DE TABLAS
    * Creación de todas las tablas
    * Corrección de errores de relacionamiento de tablas
    * Se añadió una tabla "Jefe"

--- Taller1.sql
+++ Taller1.sql
@@ -1,37 +1,61 @@
 -- Database: TALLER 1
 
--- DROP DATABASE IF EXISTS "TALLER 1";
-
-CREATE DATABASE "TALLER 1"
-    WITH
-    OWNER = postgres
-    ENCODING = 'UTF8'
-    LC_COLLATE = 'Spanish_Chile.1252'
-    LC_CTYPE = 'Spanish_Chile.1252'
-    TABLESPACE = pg_default
-    CONNECTION LIMIT = -1
-    IS_TEMPLATE = False;
-
-CREATE TABLE Sector ( 
-	Name_Sector text PRIMARY KEY,
-	km INT,
-	Client_Quantity INT	
+create table Sector(
+   name_sector text not null,
+   km int,
+   client_quantity int,
+   primary key(name_sector)
 );
 
-CREATE TABLE Client (
-	Rut text PRIMARY KEY,
-	Name text NOTNULL,
-	Address text NOTNULL,
-	Name_Sector text FOREIGN KEY
+CREATE TABLE Client(
+   rut text not null,
+   name_sector text,
+   address text not null,
+   primary key(rut),
+   foreign key (name_sector) REFERENCES Sector
 );
 
-CREATE TABLE Distribute (
-	Name_Sector_Address_Sucursal text PRIMARY KEY,
-	Name_Sector text FOREIGN KEY,
-	Address_Sucursal text FOREIGN KEY
+create table Sucursal(
+	address_sucursal text not null,
+	primary key(address_sucursal)
+
+);
+create table Worker(
+	rut text not null,
+	address_sucursal text not null,
+	primary key(rut),
+	foreign key (address_sucursal) references Sucursal
+
+);
+create table Jefe(
+	rut_jefe text not null,
+	address_sucursal text,
+	primary key(rut_jefe),
+	foreign key (address_sucursal) references Sucursal,
+	foreign key (rut_jefe) references Worker
+	
 );
 
-CREATE TABLE Sucursal (
-	Address text PRIMARY KEY,
-	Rut_manager text FOREIGN KEY
+create table Distribute(
+    id_distribute text not null,
+    name_sector text,
+    address_sucursal text not null,
+    primary key(id_distribute) ,
+    foreign key (name_sector) references Sector,
+	foreign key (address_sucursal) references Sucursal
 );
+create table Truck(
+	patente text not null,
+	primary key(patente)
+);
+
+create table Registro(
+	date_registro date not null,
+	rut text not null,
+	patente text not null,
+	primary key(date_registro),
+	foreign key (patente) references Truck,
+	foreign key (rut) references Worker
+
+);
+
