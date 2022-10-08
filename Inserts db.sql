fd06eec0ba527e35aeee4d29ff744de01ca132c1
Maria-riv <maria.rivera01@alumnos.ucn.cl>
2022-10-07 21:13:10 -0300

    INSERTS
    * INSERTS CLIENTES
    * INSERT SUCURSAL

---
+++ Inserts db.sql
@@ -0,0 +1,46 @@
+-- Relleno de datos
+
+-- SECTORES
+
+insert into sector values ('sector1','12',4);
+insert into sector values ('sector2','132',1);
+insert into sector values ('sector3','15',6);
+insert into sector values ('sector4','165',7);
+insert into sector values ('sector5','162',5);
+insert into sector values ('sector6','16',2);
+insert into sector values ('sector7','11',3);
+
+
+-- CLIENTES
+
+insert into client values ('rut1','sector1','addres1');--
+insert into client values ('rut2','sector1','addres2');
+insert into client values ('rut3','sector1','addres1');
+insert into client values ('rut4','sector1','addres4');
+insert into client values ('rut5','sector2','addres1');--
+insert into client values ('rut6','sector3','addres3');
+insert into client values ('rut7','sector3','addres6');
+insert into client values ('rut8','sector3','addres1');
+insert into client values ('rut9','sector3','addres6');
+insert into client values ('rut10','sector3','addres1');
+insert into client values ('rut11','sector3','addres6');--
+insert into client values ('rut12','sector4','addres1');
+insert into client values ('rut13','sector4','addres6');
+insert into client values ('rut14','sector4','addres1');
+insert into client values ('rut15','sector4','addres6');
+insert into client values ('rut16','sector4','addres1');
+insert into client values ('rut17','sector4','addres6');
+insert into client values ('rut18','sector4','addres1');--
+insert into client values ('rut19','sector5','addres6');
+insert into client values ('rut20','sector5','addres1');
+insert into client values ('rut21','sector5','addres6');
+insert into client values ('rut22','sector5','addres1');
+insert into client values ('rut23','sector5','addres6');
+insert into client values ('rut24','sector5','addres1');--
+insert into client values ('rut25','sector6','addres6');
+insert into client values ('rut26','sector6','addres1');
+insert into client values ('rut27','sector7','addres6');
+insert into client values ('rut28','sector7','addres1');
+insert into client values ('rut29','sector7','addres6');
+
+--SUCURSAL
